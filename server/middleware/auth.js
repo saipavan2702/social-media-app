d4cbd72f25566f039aad30f540d423f079b3ebbc
saipavan2702 <105064274+saipavan2702@users.noreply.github.com>
2023-05-23 22:51:17 +0530

    hiii

---
+++ server/middleware/auth.js
@@ -0,0 +1,20 @@
+// import { JsonWebTokenError } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+
+export const verifyToken = async (req, res, next) => {
+  try {
+    let token = req.headers("Authorization");
+    if (!token) {
+      return res.status(403).send("Access Denied");
+    }
+    if (token.startsWith("Bearer ")) {
+      token = token.slice(7, token.length).trimLeft();
+    }
+
+    const verified = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = verified;
+    next();
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
